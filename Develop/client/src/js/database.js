586ee5d36127ed62070d66dfff81aeee32340638
rmisu <roger889@umn.edu>
2022-09-24 15:05:41 -0500

    added method logic for updating and retrieving db data, added installation functionality

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb';
+import { restoreDefaultPrompts } from 'inquirer';
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -12,10 +13,34 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// DONE: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  //connect to db and version
+  const jateDb = await openDB('jate', 1);
+  //creates transaction specifying the db  and  privileges
+  const tx = jateDb.transaction('jate', 'readwrite');
+  //open object store
+  const store = tx.objectStore('jate');
+  //store.put to update data
+  const request = store.put({ id: id, contents: content });
+  //confirm request and log result
+  const result = await request;
+  console.log("Data saved to the daatabase", result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// DONE: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log("GET from the database");
+  const jateDb = await openDB('jate', 1);
+  //readonly to get data
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  //getAll to get all data from the database
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,29 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// DONE: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Stash the event so it can be triggered later. (check README)
+    window.deferredPrompt = event;
+    butInstall.style.visibility =  'visible';
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// DONE: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.defferedPrompt;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    if (!promptEvent) {
+        return;
+    };
+
+    promptEvent.prompt();
+    window.defferedPrompt = null;
+    //hides install button after user clicks
+    butInstall.classList.toggle('hidden', true);
+});
+
+// DONE: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log("Installed", 'appinstalled', event);
+    window.defferedPrompt = null;
+});
